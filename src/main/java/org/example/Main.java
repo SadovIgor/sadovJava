5e7eb75c3d3ce151753f5c8cd21bc2873ed3231d
Osegolectric <PonyQAstation@gmail.com>
2025-08-31 22:58:53 +0300

    implemented task 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,61 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Arrays;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+
+    public static void main(String[] args) {
+        getEvenNumbers();
+
+        compareStrings("sfdgasgfs", "sfdgasgfs");
+
+        makeMathOperationWith(8, 2);
+
+    }
+
+    /**
+     * Задан массив целых чисел: [1, 2, 3, 4, 5, 6, 7, 8, 9,10]
+     * необходимо написать программу, которая выведет в консоль все чётные числа.
+     */
+    public static void getEvenNumbers() {
+        int[] values = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println("Выводим в кансоль только четные числа из массива " + Arrays.toString(values));
+        for (int i = 0; i < values.length; i++) {
+            if (values[i] % 2 == 0) {
+                System.out.println(values[i]);
+            }
         }
+    }
+
+    /**
+     * Написать программу, которая принимает на вход две строки (a и b) и сравнивает их.
+     * В результате сравнения в консоль должно быть выведено одно из сообщений:
+     * "Строки неидентичны" или "Строки идентичны"
+     */
+    public static void compareStrings(String str1, String str2) {
+        if (str1.equals(str2)) {
+            System.out.println("Строки идентичны");
+        } else {
+            System.out.println("Строки неидентичны");
+        }
+    }
+
+    /**
+     * Написать программу, которая принимает на вход два целых числа (a и b) и совершает с ними следующие действия:
+     * - сравнивает эти два числа и возвращает результат сравнения путем вывода в консоль одного из вариантов: "a > b", "a < b" или "a = b";
+     * - совершает с этими числами операции сложения, вычитания, деления и умножения и результат выводит в консоль.
+     */
+    public static void makeMathOperationWith(int numb1, int numb2) {
+        if (numb1 > numb2) {
+            System.out.println(numb1 + " > " + numb2);
+        } else if (numb1 < numb2) {
+            System.out.println(numb1 + " < " + numb2);
+        } else {
+            System.out.println(numb1 + " = " + numb2);
+        }
+        System.out.println(numb1 + " + " + numb2 + " = " + (numb1 + numb2));
+        System.out.println(numb1 + " - " + numb2 + " = " + (numb1 - numb2));
+        System.out.println(numb1 + " * " + numb2 + " = " + (numb1 * numb2));
+        System.out.println(numb1 + " / " + numb2 + " = " + (numb1 / numb2));
     }
 }
